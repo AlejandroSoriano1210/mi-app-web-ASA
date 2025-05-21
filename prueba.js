3149a8e70fbf905b7bc0f2fbfc83af6b61b0e191
Alejandro Galán <alejandrogalancaballero@hotmail.com>
2025-05-21 18:30:41 +0200

    Añadiendo documentación inicial

---
+++ prueba.js
@@ -0,0 +1,7 @@
+/** Esto es un comentario de JSDoc */
+
+/** Representa un libro con dos propiedades: título y autor */
+function Libro(titulo, autor) {
+  this.titulo = titulo;
+  this.autor = autor;
+}
